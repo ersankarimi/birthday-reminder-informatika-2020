4e5a1422d0d013ca709f5a53ef9e068b499647a8
Ersan Karimi <ersanb19@gmail.com>
2022-02-26 14:23:02 +0800

    Update App.js

---
+++ src/pages/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { ContextProvider } from '../hooks/context'
+import { Header } from './../components/Header/Header'
+import { Form } from './Form'
+import { ListUsers } from './ListUsers'
+
+const App = () => {
+	return (
+		<ContextProvider>
+			<div className='container min-h-screen min-w-full p-4'>
+				<Header />
+
+				<div className='w-full m-auto p-4'>
+					<Form />
+				</div>
+
+				<div className='w-full m-auto p-4 my-4 bg  md:max-w-6xl md:grid md:gap-4 md:grid-cols-3 md:grid-rows-3 sm:grid sm:gap-3 sm:grid-cols-2 sm:grid-rows-2'>
+					<ListUsers />
+				</div>
+			</div>
+		</ContextProvider>
+	)
+}
+
+export { App }
