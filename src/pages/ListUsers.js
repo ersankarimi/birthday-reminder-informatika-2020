cae386d909db502aedcb56ceca85c4af38273379
Ersan Karimi <ersankarimii311@gmail.com>
2022-02-27 20:08:46 +0800

    Update ListUser.js
    - Mengimpor function findUserByBirthdayMonth from findUser.js
    - Update destructuring dari filterData properti
    - Membuat variabel debouncedSearchNameByMonth
    - Membuat useEffect baru untuk pencarian nama berdasarkan month / bulan

--- src/pages/ListUsers.js
+++ src/pages/ListUsers.js
@@ -3,30 +3,44 @@ import { useMyContext } from '../hooks/useMyContext'
 import { List } from './../components/List/List'
 import { Error } from './../components/Error/Error'
 import { useDebounce } from '../hooks/useDebounce'
-import { findUsersByInput } from '../utils/findUser'
+import { findUsersByInput, findUserByBirthdayMonth } from '../utils/findUser'
 
 const ListUsers = () => {
 	const {
 		data,
-		filterData: { input },
+		filterData: { input, month },
+		updateDataValue,
 	} = useMyContext()
 	const [result, setResult] = useState([])
 
 	// search name by user input
 	const [isSearching, setIsSearching] = useState(false)
-	const debouncedSearchTerm = useDebounce(input, 500)
+	const debouncedSearchNameByInput = useDebounce(input, 500)
+
+	// search name by month
+	const debouncedSearchNameByMonth = useDebounce(month, 150)
 
 	// for searh name by user input
 	useEffect(() => {
 		if (input) {
 			setIsSearching(true)
 			setResult(findUsersByInput(input, data))
+			updateDataValue('month', '')
 			setIsSearching(false)
 		} else {
 			setResult(data)
 			setIsSearching(false)
 		}
-	}, [debouncedSearchTerm])
+	}, [debouncedSearchNameByInput])
+
+	// for searh name by month
+	useEffect(() => {
+		if (month) {
+			setIsSearching(true)
+			setResult(findUserByBirthdayMonth(month, data))
+			setIsSearching(false)
+		}
+	}, [debouncedSearchNameByMonth])
 
 	return result && result.map((user) => <List {...user} key={user.id} />)
 }
