c629e3e7265a0ccf844600d69e106bf6934f4240
Ersan Karimi <ersankarimii311@gmail.com>
2022-03-05 20:54:33 +0800

    Update index.js
    - Import BrowserRouter, Route, Routes from 'react-router-dom'
    - Update ReactDom render element

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { App } from './pages/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<BrowserRouter>
+		<Routes>
+			<Route path={'/'} element={<App />}></Route>
+		</Routes>
+	</BrowserRouter>,
+	document.getElementById('root')
+)
