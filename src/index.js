d763fff8402e652816efe71bbbb9b30fda9c7c36
Ersan Karimi <ersankarimii311@gmail.com>
2022-03-07 08:45:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { App } from './pages/App'
+import { App } from './pages'
+import { Error } from './components/Error/Error'
 
 ReactDOM.render(
 	<BrowserRouter>
 		<Routes>
-			<Route path={'/'} element={<App />}></Route>
+			<Route path={'/'} element={<App />} />
+			<Route path={'*'} element={<Error />} />
 		</Routes>
 	</BrowserRouter>,
 	document.getElementById('root')
