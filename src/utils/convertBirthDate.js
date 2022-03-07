ac82bbdf794c770902b0e4dfc58d1d388ecdbee5
Ersan Karimi <ersankarimii311@gmail.com>
2022-03-07 08:51:54 +0800

    Update convertBirthDate.js

--- src/utils/convertBirthDate.js
+++ src/utils/convertBirthDate.js
@@ -1,19 +1,20 @@
 // * convert birth date to text
+export const fullMonth = {
+	'00': 'Semua',
+	'01': 'Januari',
+	'02': 'Februari',
+	'03': 'Maret',
+	'04': 'April',
+	'05': 'Mei',
+	'06': 'Juni',
+	'07': 'Juli',
+	'08': 'Agustus',
+	'09': 'September',
+	10: 'Oktober',
+	11: 'November',
+	12: 'Desember',
+}
 export const convertBirthDate = (userDate) => {
 	const [year, month, date] = userDate.split('-')
-	const fullMonth = {
-		'01': 'Januari',
-		'02': 'Febuari',
-		'03': 'Maret',
-		'04': 'April',
-		'05': 'Mei',
-		'06': 'Juni',
-		'07': 'Juli',
-		'08': 'Agustus',
-		'09': 'September',
-		10: 'Oktober',
-		11: 'November',
-		12: 'Desember',
-	}
 	return `${date} ${fullMonth[month]} ${year}`
 }
